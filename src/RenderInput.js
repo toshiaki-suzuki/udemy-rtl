6b31ea1fadbff1af502e31557cffa4f1c9f664fc
akis <flaflabell@gmail.com>
2022-03-20 19:08:15 +0900

    inputのテスト

---
+++ src/RenderInput.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+
+export const RenderInput = ({outputConsole}) => {
+  const [input, setInput] = useState("");
+  const outputValue = () => {
+    if (input) {
+      outputConsole(input);
+    } 
+  }
+  const updateValue = (e) => {
+    setInput(e.target.value);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter"
+        value={input}
+        onChange={updateValue}
+      />
+      <button onClick={outputValue}>Console</button>
+    </div>
+  )
+}
