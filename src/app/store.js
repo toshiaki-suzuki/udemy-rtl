aa64d7c1073987c6c281eb39274707207e82f77f
akis <flaflabell@gmail.com>
2022-03-21 19:21:26 +0900

    ストアにcostomReducerを追加

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import costomCounterReducer from '../features/customCounter/customCounterSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    customCounter: costomCounterReducer
   },
 });
